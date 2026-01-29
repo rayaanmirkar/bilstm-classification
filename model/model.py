ea65e741d0522b7a8339882c8607b039bce04bcf
iocle <ilmrexplorer@gmail.com>
2026-01-28 21:35:06 -0500

    Padded all 3 X sets and fixed the data conversion flow.

--- model/model.py
+++ model/model.py
@@ -7,18 +7,50 @@ from keras.layers import Bidirectional
 from keras.layers import Embedding, Dense, LSTM, Input, Conv1D, MaxPooling1D, Dropout
 from keras.models import Sequential
 
+# x = protein sequences, y = binary lifestyle
+
 input_shape = 18000
 
-df = pd.read_csv('final_data_training.csv')
 
+training_df = pd.read_csv('training_data.csv')
+testing_df = pd.read_csv('testing_data.csv')
+validation_df = pd.read_csv('validation_data.csv')
+
+'''
 raw_seqs = df['protein_sequence'].tolist
-labels = df['Temperate (empirical)'].values
+labels = df['Temperate (empirical)'].value
+'''
+# *******INITIALIZE DATAFRAMES**********
+x_training_raw = training_df['protein_sequence'].tolist
+y_training = training_df['Binary Lifestyle']
 
+x_testing_raw = testing_df['protein_sequence'].tolist
+y_testing = testing_df['Binary Lifestyle']
+
+x_validation_raw = validation_df['protein_sequence'].tolist
+y_validation = validation_df['Binary Lifestyle']
+#****************************************
+
+#*****INITIALIZE AND FIT TOKENIZER*******
+all_texts = x_testing_raw + x_training_raw + x_validation_raw
 tokenizer = Tokenizer(char_level = True, oov_token = '?')
-tokenizer.fit_on_texts(raw_seqs)
+tokenizer.fit_on_texts(all_texts)
+#***************************************
 
-int_seqs = tokenizer.texts_to_sequences(raw_seqs)
-padded_data = pad_sequences(int_seqs, maxlen = 18000, padding = "post", truncating = 'post')
+#*************Sequences to integers************************
+x_training_int = tokenizer.texts_to_sequences(x_training_raw)
+x_testing_int = tokenizer.texts_to_sequences(x_testing_raw)
+x_validation_int = tokenizer.texts_to_sequences(x_validation_raw)
+#**************************************************************
+
+
+#************PADDED SEQUENCES***********************************
+x_padded_training = pad_sequences(x_training_int, maxlen = 18000, padding = "post", truncating = 'post')
+x_padded_testing = pad_sequences(x_testing_int, maxlen = 18000, padding = "post", truncating = 'post')
+x_padded_validation = pad_sequences(x_validation_int, maxlen = 18000, padding = "post", truncating = 'post')
+#*******************************************************************************************
+
+
 
 
 #model architecture
