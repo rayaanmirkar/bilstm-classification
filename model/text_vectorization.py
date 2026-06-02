742115745bc81d1a3d4398be4de5f373e3b6c852
iocle <ilmrexplorer@gmail.com>
2026-06-02 19:23:50 -0400

    added a folder to place the deprecated models in

--- model/text_vectorization.py
+++ model/text_vectorization.py
@@ -8,6 +8,22 @@ from keras.layers import Embedding, Dense, LSTM, Input, Conv1D, MaxPooling1D, Dr
 from keras.models import Sequential
 
 max_features = 18000
+chunk = 5000
+stride_size = 5000
+
+def chunk_seq(sequence, chunk_size, stride):
+
+    chunks = []
+
+    for i in range(0, len(sequence), stride):
+
+        chunk = sequence[i:i + chunk_size]
+
+        if len(chunk) == chunk_size:
+            chunks.append(chunk)
+
+    return chunks
+
 
 training_df = pd.read_csv(r'C:\Users\\raypi\\coding\\SoftwareProjects\\phager\\building_data\\training_data.csv')
 testing_df = pd.read_csv(r'C:\Users\\raypi\\coding\\SoftwareProjects\\phager\\building_data\\testing_data.csv')
@@ -18,6 +34,9 @@ testing_df = testing_df.dropna(subset=['protein_sentence', 'Binary Lifestyle'])
 validation_df = validation_df.dropna(subset=['protein_sentence', 'Binary Lifestyle'])
 
 
+
+
+'''
 x_training = (training_df['protein_sentence']).astype(str).tolist()
 y_training = (training_df['Binary Lifestyle']).to_numpy(dtype=np.float32)
 
@@ -26,29 +45,81 @@ y_testing = testing_df['Binary Lifestyle'].to_numpy(dtype=np.float32)
 
 x_validation = validation_df['protein_sentence'].astype(str).tolist()
 y_validation = validation_df['Binary Lifestyle'].to_numpy(dtype=np.float32)
+'''
 
+
+
+x_training = []
+y_training = []
+
+for seq, label in zip(
+    training_df['protein_sentence'].astype(str),
+    training_df['Binary Lifestyle'].astype(np.float32)
+):
+
+    chunks = chunk_seq(seq, chunk, stride_size)
+
+    x_training.extend(chunks)
+
+    y_training.extend([label] * len(chunks))
+
+
+x_validation = []
+y_validation = []
+
+for seq, label in zip(
+    validation_df['protein_sentence'].astype(str),
+    validation_df['Binary Lifestyle'].astype(np.float32)
+):
+
+    chunks = chunk_seq(seq, chunk, stride_size)
+
+    x_validation.extend(chunks)
+
+    y_validation.extend([label] * len(chunks))
+
+
+x_testing = []
+y_testing = []
+
+for seq, label in zip(
+    testing_df['protein_sentence'].astype(str),
+    testing_df['Binary Lifestyle'].astype(np.float32)
+):
+
+    chunks = chunk_seq(seq, chunk, stride_size)
+
+    x_testing.extend(chunks)
+
+    y_testing.extend([label] * len(chunks))
+
+
+y_training = np.array(y_training, dtype=np.float32)
+y_validation = np.array(y_validation, dtype=np.float32)
+y_testing = np.array(y_testing, dtype=np.float32)
 
 vectorization_layer = TextVectorization(
     max_tokens= 30,
     standardize=None,
     output_mode="int",
-    output_sequence_length=50000,
+    output_sequence_length=chunk,
     split="character")
 
 vectorization_layer.adapt(x_training)
 dim_size = vectorization_layer.vocabulary_size() # maybe use vocab size method?
-
+    
 #model architecture
 model = Sequential()
 model.add(vectorization_layer)
-model.add(Embedding(input_dim=(dim_size+1), output_dim=16))
-model.add(Conv1D(filters = 128, kernel_size=15, strides=3, activation= 'relu'))
-model.add(Bidirectional(LSTM(units=128, dropout=0.2)))
+model.add(Embedding(input_dim=(dim_size+1), output_dim=32))
+model.add(Conv1D(filters = 128, kernel_size=15, strides=1, activation= 'relu'))
+model.add(Bidirectional(LSTM(units=64, dropout=0.2)))
 model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 train_ds = tf.data.Dataset.from_tensor_slices((x_training, y_training)).batch(8)
+
 val_ds = tf.data.Dataset.from_tensor_slices((x_validation, y_validation)).batch(8)
 
 
